9f849816ee98cec70e03348376e0f3d9d63d4c8b
Meiker28 <33854300+Meiker28@users.noreply.github.com>
2020-05-29 12:46:01 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
+import pandas as pd
 
 app = Flask(__name__)
 #model = pickle.load(open('model.pkl', 'rb'))
-
+df = pd.read_csv('Prediccsiones.csv')
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -12,11 +13,12 @@ def home():
 @app.route('/predict',methods=['POST'])
 def predict():
 
-    #int_features = [int(x) for x in request.form.values()]
+    int_features = [int(x) for x in request.form.values()]
     #final_features = [np.array(int_features)]
     #prediction = model.predict(final_features)
 
-    output = round(np.random.rand(), 2)*100
+    #output = round(np.random.rand(), 2)*100
+    output = round(df.loc[df.CALUMNO ==int_features[0],'PREDICHO'].values[0],3)
 
     return render_template('index.html', prediction_text='Descuento del estudiante es {}%'.format(output))
 
@@ -31,3 +33,7 @@ def results():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+#int_features = 1369260
+
+#output = round(df.loc[df.CALUMNO ==int_features,'PREDICHO'].values[0],3)
